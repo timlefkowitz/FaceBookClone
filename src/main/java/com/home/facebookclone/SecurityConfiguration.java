1d08d6357665ba486480edd2928060fb866b9557
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-24 01:10:17 -0500

    Feat: revert

--- src/main/java/com/home/facebookclone/SecurityConfiguration.java
+++ src/main/java/com/home/facebookclone/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "users/sign-up", "users/login") // anyone can see the home and the ads pages
+                .antMatchers("/", "/index", "users/sign-up", "users/login") // anyone can see these pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
