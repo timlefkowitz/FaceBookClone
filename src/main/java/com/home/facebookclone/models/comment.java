8a4eac92d6c5cbb2a7c10aae6f03aa52169f67b5
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-27 15:12:52 -0500

    Feat: friendslist

--- src/main/java/com/home/facebookclone/models/comment.java
+++ src/main/java/com/home/facebookclone/models/comment.java
@@ -66,7 +66,6 @@ public class comment {
     }
 
 
-
     /// Getters and setters
 
 
--- src/main/java/com/home/facebookclone/models/friendslist.java
+++ src/main/java/com/home/facebookclone/models/friendslist.java
@@ -5,6 +5,7 @@ import com.home.facebookclone.repos.friendslistrepo;
 
 import javax.persistence.*;
 import java.beans.ConstructorProperties;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -15,83 +16,17 @@ public class friendslist {
 
     }
 
-//    // table elements
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
-//
-////    @Column(name = "pendingFriend")           add pending friends in the future
-////    private long id;
-//
-//
-//    @Column(name = "username")
-//    private String username;
-//
-//    @OneToOne
-//    private user owner;
-//
-//    @OneToOne(cascade = CascadeType.ALL, mappedBy = "friendslist")
-//    @JoinColumn(name="friendslist")
-//    private friendslist friendslist;
-//
-//
-//
-//
-//
-//    //insert
-//
-//    public friendslist(String username, user owner) {
-//        this.username = username;
-//        this.owner = owner;
-//
-//    }
-//
-//
-//    //update
-//
-//    public friendslist(long id, String username, user owner) {
-//        this.id = id;
-//        this.username = username;
-//        this.owner = owner;
-//
-//    }
-//
-//
-//
-//
-//    //getters and setters
-//
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public user getOwner() {
-//        return owner;
-//    }
-//
-//    public void setOwner(user owner) {
-//        this.owner = owner;
-//    }
-//
-//    public com.home.facebookclone.models.friendslist getFriendslist() {
-//        return friendslist;
-//    }
-//
-//    public void setFriendslist(com.home.facebookclone.models.friendslist friendslist) {
-//        this.friendslist = friendslist;
-//    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "friends",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
+    )
+    private List<friendslist> friends = new ArrayList<>();
+
 }
