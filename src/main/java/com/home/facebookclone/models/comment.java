df7882576a24b022213a39d3d9922fd926e1879f
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-27 14:48:31 -0500

    feat: adding comment table

--- src/main/java/com/home/facebookclone/models/comment.java
+++ src/main/java/com/home/facebookclone/models/comment.java
@@ -1,10 +1,17 @@
 package com.home.facebookclone.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "comments")
 public class comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 3096)
+    private String body;
+
 }
