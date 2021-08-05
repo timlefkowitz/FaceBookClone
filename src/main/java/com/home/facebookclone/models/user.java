2b8c93a16535d872217c679b1f714a999c335c55
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-08-04 22:54:36 -0500

    YES FRIENDSLIST WORKING

--- src/main/java/com/home/facebookclone/models/user.java
+++ src/main/java/com/home/facebookclone/models/user.java
@@ -48,7 +48,7 @@ public class  user {
     private String lastName;
 
     @Column(name = "username",  length = 100)
-    private String username;     //i'm thinking this needs to be public user username;
+    public String username;     //i'm thinking this needs to be public user username;
 
     @Column(name = "email")
     private String email;
